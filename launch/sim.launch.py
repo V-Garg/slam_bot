cefd2ec7378d8c48842624f6260e4cdf27e4c09c
V-Garg <2vaibhavg@gmail.com>
2024-07-07 02:18:56 -0400

    Launch program add ons for slam

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -2,36 +2,39 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
 
-
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='slam_bot' #<--- CHANGE ME
+    package_name = 'slam_bot'
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                    get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join(
-                get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-            launch_arguments={'gz_args': 'empty.sdf'}.items()
-         )
-             
+    # Start Ignition Gazebo with the empty.world file
+    start_gazebo = ExecuteProcess(
+        cmd=['ign', 'gazebo', os.path.join(get_package_share_directory(package_name), 'worlds', 'empty.world')],
+        output='screen'
+    )
+
+    # Launch the SLAM toolbox
+    start_slam_toolbox = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('slam_toolbox'), 'launch', 'online_async_launch.py'
+        )]), launch_arguments={
+            'slam_params_file': os.path.join(get_package_share_directory(package_name), 'config', 'mapper_params_online_async.yaml'),
+            'use_sim_time': 'true'
+        }.items()
+    )
+
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='ros_ign_gazebo', executable='create',
                         arguments=['-topic', 'robot_description',
@@ -51,11 +54,11 @@ def generate_launch_description():
         output='screen'
     )
 
-
     # Launch them all!
     return LaunchDescription([
         rsp,
-        # gazebo,
+        start_gazebo,
+        start_slam_toolbox,
         spawn_entity,
         gz_bridges
     ])
